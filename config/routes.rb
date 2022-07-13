cf21c02b7fdd22dd8d6d8178a0f728e1028a7687
thimon21 <daiwamanwan-f@yahoo.co.jp>
2022-07-13 21:01:38 +0900

    #105 ユーザ登録ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :products
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
