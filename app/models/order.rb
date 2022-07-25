5ede907419f396ec918f4e721aa6ede86c0e87a8
thimon21 <daiwamanwan-f@yahoo.co.jp>
2022-07-26 08:40:56 +0900

    #107 注文詳細画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+
+  def total_price
+    order_detail.sum do |order_detail|
+      order_detail.sub_total_price
+    end
+  end
+
+  def shipment_status_exists?
+    if order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == '準備中'}
+      p "準備中"
+    else
+      p "発送済"
+    end
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,9 @@ class OrderDetail < ApplicationRecord
   belongs_to :product
   belongs_to :order
   belongs_to :shipment_status
+
+  def sub_total_price
+    product.price * order_quantity
+  end
+
 end
