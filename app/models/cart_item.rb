9e3d49d1f6f6359c89b8ae0a2585a500c604ce15
thimon21 <daiwamanwan-f@yahoo.co.jp>
2022-08-14 19:21:28 +0900

    #108 個数入力箇所のバリデーション追記

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,6 @@
 class CartItem < ApplicationRecord
+  VALID_QUANTITY_REGEX = /\A[0-9]+\z/.freeze
+
   belongs_to :cart
   belongs_to :product
 
@@ -6,4 +8,6 @@ class CartItem < ApplicationRecord
   def sum_of_price
     product.price * quantity
   end
+
+  validates :quantity, presence: true, format: { with: VALID_QUANTITY_REGEX }
 end
